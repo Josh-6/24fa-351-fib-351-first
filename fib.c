f507bca68a546dd6b1049539277a13c655f22678
Josh-6 <Joshuabarpna@gmail.com>
2024-09-07 20:34:46 -0700

    Remembering how to input and output try 1

--- fib.c
+++ fib.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
-int main() {
-   printf("Hello!\n");
+int main() 
+{
+
+   int number;
+   char letter;
+   char name;
+
+   printf("Enter a number, r or i, and a file name: \n");
+   
+
+   scanf("%d %s %s" , &number, &letter, &name);
+
+   printf("\ninputted: %d %s %s" , number, letter, name);
+
+
    return 0;
 }
