370d7d78fa0ab8de2784e02ae4444d8ecd091f66
Josh-6 <Joshuabarpna@gmail.com>
2024-09-07 22:10:45 -0700

    Code now gets user input and opens/reads file

--- fib.c
+++ fib.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() 
 {
 
-   int number;
+   int user_number;
+   int given_number;
    char letter;
    char file[10];
+   FILE* text;
 
    printf("Enter a number, r or i, and a file name: \n");
    
-   scanf("%d %c %s" , &number, &letter, file);
+   scanf("%d %c %s" , &user_number, &letter, file);
 
-   printf("\ninputted: %d %c %s\n" , number, letter, file);
+   printf("\ninputted: %d %c %s\n" , user_number, letter, file);
+
+   text = fopen(file, "r");
+
+   fscanf(text, "%d", &given_number);
+
+   printf("file number: %d\n", given_number);
 
    return 0;
 }
