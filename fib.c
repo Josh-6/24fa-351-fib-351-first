87ee0361883f59a5f85d9c0701dc238c86836858
Josh-6 <Joshuabarpna@gmail.com>
2024-09-07 21:02:42 -0700

    Able to get input and output user input, time to start actual assignment

--- fib.c
+++ fib.c
@@ -5,15 +5,13 @@ int main()
 
    int number;
    char letter;
-   char name;
+   char file[10];
 
    printf("Enter a number, r or i, and a file name: \n");
    
+   scanf("%d %c %s" , &number, &letter, file);
 
-   scanf("%d %s %s" , &number, &letter, &name);
-
-   printf("\ninputted: %d %s %s" , number, letter, name);
-
+   printf("\ninputted: %d %c %s\n" , number, letter, file);
 
    return 0;
 }
