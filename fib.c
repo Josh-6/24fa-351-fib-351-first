8e3eade76c2c8d4f1e68660ec2925b6f7f359cd1
Josh-6 <Joshuabarpna@gmail.com>
2024-09-15 14:52:25 -0700

    Finished adding memo to recursive,

--- fib.c
+++ fib.c
@@ -3,6 +3,15 @@
 #include <string.h>
 #include <inttypes.h>
 
+long long int array[94];
+
+/*int64_t fib_wrapper(int num) 
+{
+   
+
+
+   return 0;
+}*/
 int64_t iterative(int s)
 {
    int64_t  prev_num = 0;
@@ -18,14 +27,25 @@ int64_t iterative(int s)
    return current_num;
 }
 
-int64_t recursive(int64_t n)
+int64_t recursive(int n)
 {
-   if(n == 0)
+   if(n == 1)
+   {
       return 0;
-   else if(n == 1)
+   }
+   else if(n == 2)
+   {
       return 1;
+   }
+   else if(array[n] != -1)
+   {
+      return array[n];
+   }
    else
-      return (recursive(n-1) + recursive(n-2));
+   {
+      array[n] = (recursive(n - 1) + recursive(n-2));
+      return array[n];
+   }
 }
 
 int main(int argc, char* argv[]) 
@@ -34,13 +54,15 @@ int main(int argc, char* argv[])
    int user_number = atoi(argv[1]);
    int given_number;
    FILE* text;
+   FILE* saved;
    
    text = fopen(argv[3], "r");
+   saved = fopen("saved_numbers.txt", "r+");
 
    fscanf(text, "%d", &given_number);
 
    int sum = user_number + given_number;
-   printf("sum: %d\n", sum - 1);
+   printf("sum: %d\n", sum);
    int64_t fibo;
    
    if (strcmp(argv[2], "i") == 0)
@@ -49,13 +71,23 @@ int main(int argc, char* argv[])
    }
    else
    {
-      for(int i = 0; i < sum; i++)
+      /*for(int i = 1; i <= sum; i++)
       {
-         fibo = recursive(i);
+         fibo = recursive(i, array);
+         fprintf(saved, "%" PRIi64 "\n", fibo);
+         array[i] == fibo;
+      }*/
+      for (int i = 0; i <= sum; i++) 
+      {
+        array[i] = -1;
       }
+
+      fibo = recursive(sum);
    }
-   printf("%ld" PRIi64 "\n", fibo);
+   //fprintf(saved, "%d %" PRIi64 "\n", sum, fibo);
+   printf("%" PRIi64 "\n", fibo);
    fclose(text);
+   fclose(saved);
    
    return 0;
 }
